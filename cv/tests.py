3afec94b494cbac3ed78ba3cd5dd1db5c4602c3b
MarchiT <mrtihova@gmail.com>
2020-08-21 21:15:19 +0100

    Edit preview cv tests according to the new models

--- cv/tests.py
+++ cv/tests.py
@@ -2,25 +2,15 @@ from django.test import TestCase
 
 from django.urls import reverse
 
-from .models import Credentials, CV, Qualifications, Qualification
+from .models import Credentials, Qualification
 
-t = Qualifications.QTypes
+q_types = Qualification.QTypes.values
 
 
 def create_cv():
-    credentials = Credentials.objects.create(name='Marina', address='Test Address',
-                                             phone='+1234', email='marina@example.com')
-    cv = CV.objects.create(credentials=credentials)
-
-    types = [{'type': v, 'on_cv_id': cv.id} for v in t.values]
-    cv.qualifications_set.bulk_create(Qualifications(**q) for q in types)
-
-    for qt in t.values:
-        qs = cv.qualifications_set.get(type=qt)
-        data = [{'text': qt + str(i), 'qualifications_id': qs.id} for i in range(3)]
-        qs.qualification_set.bulk_create(Qualification(**q) for q in data)
-
-    return cv
+    for t in q_types:
+        [Qualification(type=t, text=t + ' qualification #' + str(x)).save() for x in range(5)]
+    return Credentials.objects.create(name='Marina', address='Test Address', phone='+1234', email='marina@example.com')
 
 
 class CVPreviewTests(TestCase):
@@ -32,7 +22,7 @@ class CVPreviewTests(TestCase):
     def test_preview_page_without_cv(self):
         response = self.client.get(reverse('cv:preview'))
         self.assertEqual(response.status_code, 200)
-        self.assertIs(response.context['cv'], None)
+        self.assertIs(response.context['credentials'], None)
         self.assertContains(response, 'CV not available.')
 
     def test_preview_page_with_cv(self):
@@ -42,36 +32,45 @@ class CVPreviewTests(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertNotContains(response, 'CV not available.')
         # assert all section names are present
-        for q_type in t.names:
-            self.assertContains(response, q_type)
+        for t in q_types:
+            self.assertContains(response, t)
 
     def test_cv_credentials_should_be_displayed(self):
-        cv = create_cv()
+        credentials = create_cv()
         response = self.client.get(reverse('cv:preview'))
 
-        self.assertContains(response, cv.credentials.name)
-        self.assertContains(response, cv.credentials.phone)
-        self.assertContains(response, cv.credentials.email)
-        self.assertContains(response, cv.credentials.address)
+        self.assertContains(response, credentials.name)
+        self.assertContains(response, credentials.phone)
+        self.assertContains(response, credentials.email)
+        self.assertContains(response, credentials.address)
 
     def test_cv_qualifications_should_be_displayed(self):
-        cv = create_cv()
+        create_cv()
         response = self.client.get(reverse('cv:preview'))
 
-        for qs in cv.qualifications_set.all():
-            for q in qs.qualification_set.all():
-                self.assertContains(response, q.text)
+        for q in Qualification.objects.all():
+            self.assertContains(response, q.text)
 
 
 class CVEditTests(TestCase):
     def test_uses_edit_template(self):
-        pass
+        response = self.client.get(reverse('cv:edit'))
+        self.assertTemplateUsed(response, 'cv/edit.html')
 
     def test_edit_without_cv(self):
+        # must have 'create' button instead
+        response = self.client.get(reverse('cv:edit'))
         pass
 
     def test_all_cv_sections_should_be_present(self):
-        pass
+        create_cv()
+        response = self.client.get(reverse('cv:edit'))
+
+        self.assertEqual(response.status_code, 200)
+        self.assertNotContains(response, 'CV not available.')
+        # assert all section names are present
+        for t in q_types:
+            self.assertContains(response, t)
 
     def test_can_save_POST_request(self):
         pass
